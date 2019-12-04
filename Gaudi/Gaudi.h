b2fe288ca2fb14450edd4fe8bd0684954f9ce45b
Giuseppe Lanza <gringoire986@gmail.com>
2019-12-04 07:32:41 +0000

    Initial Commit

---
+++ Gaudi/Gaudi.h
@@ -0,0 +1,19 @@
+//
+//  Gaudi.h
+//  Gaudi
+//
+//  Created by Giuseppe Lanza on 04/12/2019.
+//  Copyright © 2019 Giuseppe Lanza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gaudi.
+FOUNDATION_EXPORT double GaudiVersionNumber;
+
+//! Project version string for Gaudi.
+FOUNDATION_EXPORT const unsigned char GaudiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gaudi/PublicHeader.h>
+
+
